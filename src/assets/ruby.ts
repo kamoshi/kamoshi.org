bc8c817e921bc8f72233a58efa57dcf0d39220d0
Maciej Jur <maciej@kamoshi.org>
2023-04-12 02:02:46 +0200

    Hand roll a SSR markdown parser for reveal

--- src/assets/ruby.ts
+++ src/assets/ruby.ts
@@ -1,19 +1,24 @@
+import type { Node, Parent } from 'unist';
 import { visit } from "unist-util-visit";
-import type { Node } from "unist-util-visit/lib";
 
+
+interface RubyNode extends Node {
+  type: 'html';
+  value: string;
+}
 
 const regex = /\{.+?\}\(.+?\)/g;
 const group = /^\{(.+?)\}\((.+?)\)$/;
 const template = "<ruby>$1<rp>(</rp><rt>$2</rt><rp>)</rp></ruby>";
 
-function toRuby(ruby: string) {
-  return ({
+function toRuby(ruby: string): RubyNode {
+  return {
     type: "html",
     value: ruby.replace(group, template),
-  })
+  }
 }
 
-function transformRuby(node: { value: string }, index: number, parent: any) {
+function transformRuby(node: {value: string}, index: number, parent: Parent) {
   if (!regex.test(node.value)) return;
 
   const text = node.value.split(regex).map(value => ({ type: "text", value}));
