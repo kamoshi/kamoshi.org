0edf089791a6f6f1edf69f357d35ba33d7d32a91
Maciej Jur <maciej@kamoshi.org>
2023-06-11 12:46:48 +0200

    (build) less verbose rsync

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pnpm build
-rsync -Pavz -e "ssh -i ~/.ssh/id_ed25519" ./dist/ 70.34.244.173:/var/www/kamoshi.org --delete
+rsync -qi -Pavz -e "ssh -i ~/.ssh/id_ed25519" ./dist/ 70.34.244.173:/var/www/kamoshi.org --delete
 echo "Done"
