700a93a5ee3c5f731e13a824321170bbb63a3ff3
Maciej Jur <maciej@kamoshi.org>
2024-04-30 23:42:30 +0200

    feat: update treesitter to v22

--- src/ts/captures.rs
+++ src/ts/captures.rs
@@ -1,10 +1,11 @@
 /// Source: https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md
 pub const NAMES: &[&str] = &[
     // Identifiers
-    "variable",             // various variable names
-    "variable.builtin",     // built-in variable names (e.g. `this`)
-    "variable.parameter",   // parameters of a function
-    "variable.member",      // object and struct fields
+    "variable",                     // various variable names
+    "variable.builtin",             // built-in variable names (e.g. `this`)
+    "variable.parameter",           // parameters of a function
+    "variable.parameter.builtin",   // special parameters (e.g. `_`, `it`)
+    "variable.member",              // object and struct fields
 
     "constant",             // constant identifiers
     "constant.builtin",     // built-in constant values
@@ -35,9 +36,9 @@ pub const NAMES: &[&str] = &[
     "type",                 // type or class definitions and annotations
     "type.builtin",         // built-in types
     "type.definition",      // identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
-    "type.qualifier",       // type qualifiers (e.g. `const`)
 
     "attribute",            // attribute annotations (e.g. Python decorators)
+    "attribute.builtin",    // builtin annotations (e.g. `@property` in Python)
     "property",             // the key in key/value pairs
 
     // Functions
@@ -58,61 +59,68 @@ pub const NAMES: &[&str] = &[
     "keyword.function",     // keywords that define a function (e.g. `func` in Go, `def` in Python)
     "keyword.operator",     // operators that are English words (e.g. `and` / `or`)
     "keyword.import",       // keywords for including modules (e.g. `import` / `from` in Python)
-    "keyword.storage",      // modifiers that affect storage in memory or life-time
+    "keyword.type",         // keywords describing composite types (e.g. `struct`, `enum`)
+    "keyword.modifier",     // keywords modifying other constructs (e.g. `const`, `static`, `public`)
     "keyword.repeat",       // keywords related to loops (e.g. `for` / `while`)
     "keyword.return",       // keywords like `return` and `yield`
     "keyword.debug",        // keywords related to debugging
     "keyword.exception",    // keywords related to exceptions (e.g. `throw` / `catch`)
 
-    "keyword.conditional",         // keywords related to conditionals (e.g. `if` / `else`)
-    "keyword.conditional.ternary", // ternary operator (e.g. `?` / `:`)
+    "keyword.conditional",          // keywords related to conditionals (e.g. `if` / `else`)
+    "keyword.conditional.ternary",  // ternary operator (e.g. `?` / `:`)
 
     "keyword.directive",        // various preprocessor directives & shebangs
     "keyword.directive.define", // preprocessor definition directives
 
     // Punctuation
-    "punctuation.delimiter", // delimiters (e.g. `;` / `.` / `,`)
-    "punctuation.bracket",   // brackets (e.g. `()` / `{}` / `[]`)
-    "punctuation.special",   // special symbols (e.g. `{}` in string interpolation)
+    "punctuation.delimiter",    // delimiters (e.g. `;` / `.` / `,`)
+    "punctuation.bracket",      // brackets (e.g. `()` / `{}` / `[]`)
+    "punctuation.special",      // special symbols (e.g. `{}` in string interpolation)
 
     // Comments
-    "comment",               // line and block comments
-    "comment.documentation", // comments documenting code
+    "comment",                  // line and block comments
+    "comment.documentation",    // comments documenting code
 
-    "comment.error",         // error-type comments (e.g. `ERROR`, `FIXME`, `DEPRECATED:`)
-    "comment.warning",       // warning-type comments (e.g. `WARNING:`, `FIX:`, `HACK:`)
-    "comment.todo",          // todo-type comments (e.g. `TODO:`, `WIP:`, `FIXME:`)
-    "comment.note",          // note-type comments (e.g. `NOTE:`, `INFO:`, `XXX`)
+    "comment.error",        // error-type comments (e.g. `ERROR`, `FIXME`, `DEPRECATED:`)
+    "comment.warning",      // warning-type comments (e.g. `WARNING:`, `FIX:`, `HACK:`)
+    "comment.todo",         // todo-type comments (e.g. `TODO:`, `WIP:`, `FIXME:`)
+    "comment.note",         // note-type comments (e.g. `NOTE:`, `INFO:`, `XXX`)
 
     // Markup
-    "markup.strong",         // bold text
-    "markup.italic",         // italic text
-    "markup.strikethrough",  // struck-through text
-    "markup.underline",      // underlined text (only for literal underline markup!)
+    "markup.strong",        // bold text
+    "markup.italic",        // italic text
+    "markup.strikethrough", // struck-through text
+    "markup.underline",     // underlined text (only for literal underline markup!)
 
-    "markup.heading",        // headings, titles (including markers)
+    "markup.heading",       // headings, titles (including markers)
+    "markup.heading.1",     // top-level heading
+    "markup.heading.2",     // section heading
+    "markup.heading.3",     // subsection heading
+    "markup.heading.4",     // and so on
+    "markup.heading.5",     // and so forth
+    "markup.heading.6",     // six levels ought to be enough for anybody
 
-    "markup.quote",          // block quotes
-    "markup.math",           // math environments (e.g. `$ ... $` in LaTeX)
-    "markup.environment",    // environments (e.g. in LaTeX)
+    "markup.quote",         // block quotes
+    "markup.math",          // math environments (e.g. `$ ... $` in LaTeX)
 
-    "markup.link",           // text references, footnotes, citations, etc.
-    "markup.link.label",     // link, reference descriptions
-    "markup.link.url",       // URL-style links
+    "markup.link",          // text references, footnotes, citations, etc.
+    "markup.link.label",    // link, reference descriptions
+    "markup.link.url",      // URL-style links
 
-    "markup.raw",            // literal or verbatim text (e.g. inline code)
-    "markup.raw.block",      // literal or verbatim text as a stand-alone block
-                             // (use priority 90 for blocks with injections)
+    "markup.raw",           // literal or verbatim text (e.g. inline code)
+    "markup.raw.block",     // literal or verbatim text as a stand-alone block
+                            // (use priority 90 for blocks with injections)
 
-    "markup.list",           // list markers
-    "markup.list.checked",   // checked todo-style list markers
-    "markup.list.unchecked", // unchecked todo-style list markers
+    "markup.list",              // list markers
+    "markup.list.checked",      // checked todo-style list markers
+    "markup.list.unchecked",    // unchecked todo-style list markers
 
-    "diff.plus",             // added text (for diff files)
-    "diff.minus",            // deleted text (for diff files)
-    "diff.delta",            // changed text (for diff files)
+    "diff.plus",            // added text (for diff files)
+    "diff.minus",           // deleted text (for diff files)
+    "diff.delta",           // changed text (for diff files)
 
-    "tag",                   // XML-style tag names (and similar)
-    "tag.attribute",         // XML-style tag attributes
-    "tag.delimiter",         // XML-style tag delimiters
+    "tag",                  // XML-style tag names (and similar)
+    "tag.builtin",          // builtin tag names (e.g. HTML5 tags)
+    "tag.attribute",        // XML-style tag attributes
+    "tag.delimiter",        // XML-style tag delimiters
 ];
--- src/ts/configs.rs
+++ src/ts/configs.rs
@@ -24,66 +24,70 @@ pub static EXTENSIONS: Lazy<HashMap<&'static str, &'static str>> = Lazy::new(|| 
         ("md", "markdown"),
         ("mdx", "markdown"),
         ("py", "python"),
-        ("query", "scheme"),
-        ("scm", "scheme"),
-        ("scss", "css"),
-        ("ts", "typescript")
+        ("query", "query"),
+        // ("scm", "scheme"),
+        ("scss", "scss"),
+        ("ts", "javascript"),
+        ("typescript", "javascript")
     ])
 });
 
 fn config_for(
     lang: Language,
+    name: &str,
     highlights: &str,
     injections: &str,
     locals: &str,
 ) -> HighlightConfiguration {
-    let mut config = HighlightConfiguration::new(lang, highlights, injections, locals).unwrap();
+    let mut config = HighlightConfiguration::new(lang, name, highlights, injections, locals).unwrap();
     config.configure(captures::NAMES);
     config
 }
 
 pub static CONFIGS: Lazy<HashMap<&'static str, HighlightConfiguration>> = Lazy::new(|| {
     HashMap::from([
-        (
-            "astro",
-            config_for(
-                tree_sitter_astro::language(),
-                query!("astro/highlights"),
-                query!("astro/injections"),
-                "",
-            )
-        ),
-        (
-            "css",
-            config_for(
-                tree_sitter_css::language(),
-                query!("css/highlights"),
-                "",
-                "",
-            )
-        ),
+        // (
+        //     "astro",
+        //     config_for(
+        //         tree_sitter_astro::language(),
+        //         query!("astro/highlights"),
+        //         query!("astro/injections"),
+        //         "",
+        //     )
+        // ),
+        // (
+        //     "css",
+        //     config_for(
+        //         tree_sitter_css::language(),
+        //         query!("css/highlights"),
+        //         "",
+        //         "",
+        //     )
+        // ),
         (
             "haskell",
             config_for(
-                tree_sitter_haskell::language(),
-                tree_sitter_haskell::HIGHLIGHTS_QUERY,
+                npezza93_tree_sitter_haskell::language(),
+                "haskell",
+                npezza93_tree_sitter_haskell::HIGHLIGHTS_QUERY,
                 "",
-                tree_sitter_haskell::LOCALS_QUERY,
+                npezza93_tree_sitter_haskell::LOCALS_QUERY,
             )
         ),
-        (
-            "html",
-            config_for(
-                tree_sitter_html::language(),
-                tree_sitter_html::HIGHLIGHTS_QUERY,
-                tree_sitter_html::INJECTIONS_QUERY,
-                "",
-            )
-        ),
+        // (
+        //     "html",
+        //     config_for(
+        //         tree_sitter_html::language(),
+        //         tree_sitter_html::HIGHLIGHTS_QUERY,
+        //         tree_sitter_html::INJECTIONS_QUERY,
+        //         "",
+        //     )
+        // ),
         (
             "javascript",
             config_for(
                 tree_sitter_javascript::language(),
+                "javascript",
                 &format!("{} {}",
                     query!("ecma/highlights"),
                     tree_sitter_javascript::HIGHLIGHT_QUERY,
@@ -96,6 +100,7 @@ pub static CONFIGS: Lazy<HashMap<&'static str, HighlightConfiguration>> = Lazy::
             "jsx",
             config_for(
                 tree_sitter_javascript::language(),
+                "jsx",
                 &format!("{} {} {}",
                     query!("ecma/highlights"),
                     tree_sitter_javascript::HIGHLIGHT_QUERY,
@@ -109,6 +114,7 @@ pub static CONFIGS: Lazy<HashMap<&'static str, HighlightConfiguration>> = Lazy::
             "markdown",
             config_for(
                 tree_sitter_md::language(),
+                "markdown",
                 tree_sitter_md::HIGHLIGHT_QUERY_BLOCK,
                 tree_sitter_md::INJECTION_QUERY_BLOCK,
                 "",
@@ -118,80 +124,92 @@ pub static CONFIGS: Lazy<HashMap<&'static str, HighlightConfiguration>> = Lazy::
             "python",
             config_for(
                 tree_sitter_python::language(),
-                tree_sitter_python::HIGHLIGHT_QUERY,
+                "python",
+                tree_sitter_python::HIGHLIGHTS_QUERY,
                 "",
                 "",
             )
         ),
         (
-            "regex",
+            "query",
             config_for(
-                tree_sitter_regex::language(),
-                query!("regex/highlights"),
-                "",
+                tree_sitter_query::language(),
+                "query",
+                tree_sitter_query::HIGHLIGHTS_QUERY,
+                tree_sitter_query::INJECTIONS_QUERY,
                 "",
             )
         ),
+        // (
+        //     "regex",
+        //     config_for(
+        //         tree_sitter_regex::language(),
+        //         query!("regex/highlights"),
+        //         "",
+        //         "",
+        //     )
+        // ),
         (
             "rust",
             config_for(
                 tree_sitter_rust::language(),
-                tree_sitter_rust::HIGHLIGHT_QUERY,
+                "rust",
+                tree_sitter_rust::HIGHLIGHTS_QUERY,
                 tree_sitter_rust::INJECTIONS_QUERY,
                 "",
             )
         ),
-        (
-            "scheme",
-            config_for(
-                tree_sitter_scheme::language(),
-                tree_sitter_scheme::HIGHLIGHTS_QUERY,
-                "",
-                "",
-            )
-        ),
-        (
-            "toml",
-            config_for(
-                tree_sitter_toml::language(),
-                tree_sitter_toml::HIGHLIGHT_QUERY,
-                "",
-                "",
-            )
-        ),
-        (
-            "tsx",
-            config_for(
-                tree_sitter_typescript::language_tsx(),
-                &format!("{} {} {} {}",
-                    query!("ecma/highlights"),
-                    tree_sitter_javascript::HIGHLIGHT_QUERY,
-                    tree_sitter_javascript::JSX_HIGHLIGHT_QUERY,
-                    tree_sitter_typescript::HIGHLIGHT_QUERY,
-                ),
-                tree_sitter_javascript::INJECTION_QUERY,
-                &format!("{} {}",
-                    tree_sitter_javascript::LOCALS_QUERY,
-                    tree_sitter_typescript::LOCALS_QUERY
-                )
-            )
-        ),
-        (
-            "typescript",
-            config_for(
-                tree_sitter_typescript::language_typescript(),
-                &format!("{} {} {}",
-                    query!("ecma/highlights"),
-                    tree_sitter_javascript::HIGHLIGHT_QUERY,
-                    tree_sitter_typescript::HIGHLIGHT_QUERY,
-                ),
-                tree_sitter_javascript::INJECTION_QUERY,
-                &format!("{} {}",
-                    tree_sitter_javascript::LOCALS_QUERY,
-                    tree_sitter_typescript::LOCALS_QUERY
-                ),
-            )
-        ),
+        // (
+        //     "scheme",
+        //     config_for(
+        //         tree_sitter_scheme::language(),
+        //         tree_sitter_scheme::HIGHLIGHTS_QUERY,
+        //         "",
+        //         "",
+        //     )
+        // ),
+        // (
+        //     "toml",
+        //     config_for(
+        //         tree_sitter_toml::language(),
+        //         tree_sitter_toml::HIGHLIGHT_QUERY,
+        //         "",
+        //         "",
+        //     )
+        // ),
+        // (
+        //     "tsx",
+        //     config_for(
+        //         tree_sitter_typescript::language_tsx(),
+        //         &format!("{} {} {} {}",
+        //             query!("ecma/highlights"),
+        //             tree_sitter_javascript::HIGHLIGHT_QUERY,
+        //             tree_sitter_javascript::JSX_HIGHLIGHT_QUERY,
+        //             tree_sitter_typescript::HIGHLIGHT_QUERY,
+        //         ),
+        //         tree_sitter_javascript::INJECTION_QUERY,
+        //         &format!("{} {}",
+        //             tree_sitter_javascript::LOCALS_QUERY,
+        //             tree_sitter_typescript::LOCALS_QUERY
+        //         )
+        //     )
+        // ),
+        // (
+        //     "typescript",
+        //     config_for(
+        //         tree_sitter_typescript::language_typescript(),
+        //         &format!("{} {} {}",
+        //             query!("ecma/highlights"),
+        //             tree_sitter_javascript::HIGHLIGHT_QUERY,
+        //             tree_sitter_typescript::HIGHLIGHT_QUERY,
+        //         ),
+        //         tree_sitter_javascript::INJECTION_QUERY,
+        //         &format!("{} {}",
+        //             tree_sitter_javascript::LOCALS_QUERY,
+        //             tree_sitter_typescript::LOCALS_QUERY
+        //         ),
+        //     )
+        // ),
     ])
 });
 
